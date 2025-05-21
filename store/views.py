8dc036de324db0dd84c6090d1319ca94b41645ea
Cullinan-coder <kocasamet16@gmail.com>
2025-05-22 01:41:39 +0300

    güncellendi

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Altınyıldız Clone yayında usta! 🚀")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),  # store uygulamasındaki url'leri dahil eder
